a60c5cde1a9cedcdaa5a1eb1fccae34fac63f146
Galina GCAS <galina.gcas@gmail.com>
2025-09-13 21:45:43 +0300

    fixed images

--- vite.config.js
+++ vite.config.js
@@ -19,5 +19,7 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom', 'framer-motion']
-  }
+  },
+  publicDir: 'public',
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.svg']
 })
