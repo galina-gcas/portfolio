c6f18b3d3e8e71b624fbfc4ad804198bfa338335
Galina GCAS <galina.gcas@gmail.com>
2025-09-13 21:13:41 +0300

    fixed path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Правильный путь для вашего репозитория
+  base: '/portfolio/', // Правильный путь для репозитория portfolio
   build: {
     outDir: 'dist',
     rollupOptions: {
